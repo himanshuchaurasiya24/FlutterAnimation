746aebcf157ee23f6b5f6f87f3d25d3a57635ada
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2024-05-25 19:31:54 +0530

    list_animation_done

--- animations_starter_code/lib/explicit_examples/list_animation.dart
+++ animations_starter_code/lib/explicit_examples/list_animation.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
 
-class ListAnimation extends StatelessWidget {
+class ListAnimation extends StatefulWidget {
   const ListAnimation({super.key});
+
+  @override
+  State<ListAnimation> createState() => _ListAnimationState();
+}
+
+class _ListAnimationState extends State<ListAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late List<Animation<Offset>> animations;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+    animations = List.generate(
+      5,
+      (index) => Tween<Offset>(
+        begin: const Offset(-1, 0),
+        end: Offset.zero,
+      ).animate(CurvedAnimation(
+        parent: controller,
+        curve: Interval(
+          index * (1 / 5),
+          1,
+          curve: Curves.bounceIn,
+        ),
+      )),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +43,22 @@ class ListAnimation extends StatelessWidget {
       body: ListView.builder(
         itemCount: 5,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Hello World, Rivaan. ${index.toString()}'),
+          return SlideTransition(
+            position: animations[index],
+            child: ListTile(
+              title: Text('Hello World, Himanshu. ${index.toString()}'),
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          if (controller.isCompleted) {
+            controller.reverse();
+          } else {
+            controller.forward();
+          }
+        },
         child: const Icon(Icons.done),
       ),
     );
